f11c50456141f10153b48dc8276df60968969b79
Jonghakseo <jonghak.seo@creatrip.com>
2024-07-22 18:19:03 +0900

    fix: ui size when rem based on 62.5%

--- src/pages/content/src/ContentScriptApp/App.tsx
+++ src/pages/content/src/ContentScriptApp/App.tsx
@@ -4,12 +4,27 @@ import ResetStyleProvider from "@pages/content/src/ContentScriptApp/emotion/Rese
 import FontProvider from "@pages/content/src/ContentScriptApp/emotion/FontProvider";
 import { CSSReset, theme, ThemeProvider } from "@chakra-ui/react";
 
+theme.space;
 export default function App() {
   return (
     <ResetStyleProvider>
       <FontProvider>
         <EmotionCacheProvider>
-          <ThemeProvider theme={theme}>
+          <ThemeProvider
+            theme={{
+              ...theme,
+              fontSizes: {
+                ...theme.fontSizes,
+                sm: "14px",
+                xs: "12px",
+              },
+              sizes: {
+                ...theme.sizes,
+                6: "24px",
+                8: "32px",
+              },
+            }}
+          >
             <CSSReset />
             <DragGPT />
           </ThemeProvider>
